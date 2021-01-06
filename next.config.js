2ad18d49fcfc339fb7ac72df5ccecc575d88e2e8
Alfredo Patricius Tarigan <alfredoptarigan@gmail.com>
2021-01-06 19:50:27 +0700

    Update 1.0

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const withPWA = require("next-pwa");
+
+module.exports = {
+  pwa: {
+    dest: "public",
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,39 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Weather &mdash; Application</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="Weather Application for Final Project Rekayasa Perangkat Lunak"
+        />
+        <meta
+          name="keywords"
+          content="weather weather-alfredo weather-labd3ti"
+        />
+
+        <link rel="manifest" href="/dunplab-manifest-29805" />
+        <link
+          href="/favicon-16x16-dunplab-manifest-29805.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/favicon-32x32-dunplab-manifest-29805.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon-57x57-dunplab-manifest-29805.png"
+        ></link>
+        <meta name="theme-color" content="#2196f3" />
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
